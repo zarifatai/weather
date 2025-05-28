922a843ab7454cdf8c915eb255fff33a3f81205b
Zarif Atai <z.atai@protonmail.com>
2025-05-28 16:25:42 +0200

    Added trait for printing conditions

--- src/models/weather_api.rs
+++ src/models/weather_api.rs
@@ -12,11 +12,13 @@ pub struct Location {
     pub name: String,
     pub region: String,
     pub country: String,
+    pub localtime: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CurrentConditions {
     pub last_updated_epoch: i32,
+    pub last_updated: String,
     pub temp_c: f32,
     pub condition: ConditionText,
     pub wind_kph: f32,
@@ -61,3 +63,36 @@ pub struct ForecastDay {
     pub day: DayConditions,
     pub hour: Vec<HourConditions>,
 }
+
+pub trait CommonConditionsAttributes {
+    fn feelslike_c(&self) -> &f32;
+    fn condition(&self) -> &ConditionText;
+    fn temp_c(&self) -> &f32;
+    fn uv(&self) -> &f32;
+    fn wind_kph(&self) -> &f32;
+}
+
+macro_rules! impl_common_conditions_attributes {
+    ($t:ty) => {
+        impl CommonConditionsAttributes for $t {
+            fn feelslike_c(&self) -> &f32 {
+                &self.feelslike_c
+            }
+            fn condition(&self) -> &ConditionText {
+                &self.condition
+            }
+            fn temp_c(&self) -> &f32 {
+                &self.temp_c
+            }
+            fn uv(&self) -> &f32 {
+                &self.uv
+            }
+            fn wind_kph(&self) -> &f32 {
+                &self.wind_kph
+            }
+        }
+    };
+}
+
+impl_common_conditions_attributes!(CurrentConditions);
+impl_common_conditions_attributes!(HourConditions);
