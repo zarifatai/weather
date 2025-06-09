c07b1f104107c9aff629c0a5d72030d3278bbd94
Zarif Atai <z.atai@protonmail.com>
2025-06-10 00:01:05 +0200

    Incremented n_days so that it does not include today date

--- src/api.rs
+++ src/api.rs
@@ -6,6 +6,8 @@ pub async fn request_weather_data(
     city: String,
     n_days: &i32,
 ) -> Result<models::weather_api::Response, Box<dyn std::error::Error>> {
+    // By n_days the current date is excluded so we increment it by 1
+    let n_days = n_days + 1;
     let url = format!(
         "https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days={n_days}&aqi=no&alerts=no"
     );
